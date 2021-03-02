9f4b89b7eb8e6faf6e81480c6c3a9c202c3a65ff
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-02 12:06:29 +0100

    solving ex-1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,26 @@
 // Use this function that is linked to the button to change the background
 
-function set_background() {
 
-}
+  function set_background() {
+    var p1 = document.querySelectorAll("p")[0];
+    p1.style.backgroundColor = "red";
+    var p2 = document.querySelectorAll("p")[1];
+    p2.style.backgroundColor = "blue";
+    var p3 = document.querySelectorAll("p")[2];
+    p3.style.backgroundColor = "green";
+    } 
+
+    // 2nd way
+
+    function set_background() {
+        var p1 = document.getElementsByTagName("p")[0];
+        p1.style.backgroundColor = "yellow";
+        console.log(p1);
+        var p2 = document.getElementsByTagName("p")[1];
+        console.log(p2.textContent);
+        p2.style.backgroundColor = "pink";
+        console.log(p2);
+        var p3 = document.getElementsByTagName("p")[2];
+        p3.style.backgroundColor = "red";
+        console.log(p3);
+        }
