053521fac7d02a84fba5abb985cedc59fe4bac49
Mohand Abboud <mohandabboud9@gmail.com>
2021-03-02 15:11:55 +0100

    solving ex-4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,22 @@
+/* function removecolor() {
+    let parent = document.querySelector("#colorSelect");
+    let child = parent.querySelector("option");
+    console.log(child)
+    parent.removeChild(child);
+
+} */
+
+/* function removecolor() {
+    let parent = document.querySelector("#colorSelect");
+   
+    if (select = parent[0]) {
+    parent.removeChild(parent[0]);}
+    else {parent.removeChild(parent[1]);}
+} */
+
 function removecolor() {
+    let element = document.querySelector("#colorSelect option:checked");
+    if (element) element.remove();
 
 }
+
